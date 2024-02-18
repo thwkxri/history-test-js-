9da1c09f652087ed76185e076a0421ed363dff85
Dmitry <shoex64@gmail.com>
2024-02-18 12:54:24 +0300

    answers

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 function history_test() {
-  let score = '';
+  let score = 0;
   if (document.getElementsByName('q1')[0].checked) {
-    score = '1';
+    score++;
   }
-
-  document.getElementById('score').textContent = `Правильных ответов: ${score}`;
+  if (document.getElementsByName('q2')[2].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q3')[1].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q4')[2].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q5')[2].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q6')[0].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q7')[0].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q8')[3].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q9')[3].checked) {
+    score++;
+  }
+  if (document.getElementsByName('q10')[0].checked) {
+    score++;
+  }
+  document.getElementById('score').textContent = `Правильных ответов: ${score}/10`;
 }
--- тест на js/script.js
+++
@@ -1,8 +0,0 @@
-function history_test() {
-  let score = '';
-  if (document.getElementsByName('q1')[0].checked) {
-    score = '1';
-  }
-
-  document.getElementById('score').textContent = `Правильных ответов: ${score}`;
-}
